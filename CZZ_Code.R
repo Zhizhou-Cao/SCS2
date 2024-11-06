9ae2a3710ce7a836a56cc2d0c39859793722a0f3
Joy Cao <s2178688@ed.ac.uk>
2024-11-06 14:38:56 +0000

    1

--- CZZ_Code.R
+++ CZZ_Code.R
@@ -12,19 +12,27 @@ humanM <- loadCorpus("functionwords/functionwords/humanfunctionwords/","function
 GPTM <- loadCorpus("functionwords/functionwords/GPTfunctionwords/","functionwords")
 
 #select all essays
-humanfeatures <- humanM$features
-GPTfeatures <- GPTM$features
+#humanfeatures <- humanM$features[[1]]
+#GPTfeatures <- GPTM$features[[1]]
 
 #this is a matrix of both human and GPT essays
-features <- rbind(humanfeatures, GPTfeatures) 
+#features <- rbind(humanfeatures, GPTfeatures) 
 
 #here i use author=0 for human, and author=1 for ChatGPT
-authornames <- c(rep(0,nrow(humanfeatures)), rep(1,nrow(GPTfeatures)))
-
+#authornames <- c(rep(0,length(humanfeatures)), rep(1,length(GPTfeatures)))
 # Q1 Human Or Chatgpt?------
 
 
 
+human_matrix <- do.call(rbind, humanfeatures)
+GPT_matrix <- do.call(rbind, GPTfeatures)
+features <- rbind(human_matrix, GPT_matrix)
+authornames <- c(rep(0,nrow(human_matrix)), rep(1,nrow(GPT_matrix)))
 
+for (i in 1:nrow(features)) {
+  features[i, ] <- features[i, ] / sum(features[i, ])
+}
+
+HG <- list(features = list(features), authornames = list(authornames))
 
 
