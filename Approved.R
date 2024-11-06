61325fba862467349db1cd378b8118ac7e7b487a
Joy Cao <s2178688@ed.ac.uk>
2024-11-06 15:54:54 +0000

    1

--- Approved.R
+++ Approved.R
@@ -15,14 +15,6 @@ GPTM <- loadCorpus("functionwords/functionwords/GPTfunctionwords/","functionword
 humanM$authornames <- rep(0, length(humanM$authornames))
 GPTM$authornames <- rep(1, length(GPTM$authornames))
 
-# 合并列表
-combined_features <- c(humanM$features, GPTM$features)
-combined_authornames <- c(humanM$authornames, GPTM$authornames)
-combined_booknames <- c(humanM$booknames, GPTM$booknames)
-combined_list <- list(features = combined_features, 
-                      authornames = combined_authornames, 
-                      booknames = combined_booknames)
-
 # Create a new list for Q1
 combined_q1 <- list(
   features = list(
